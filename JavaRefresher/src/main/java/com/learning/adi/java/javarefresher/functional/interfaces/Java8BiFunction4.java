f57a51d9e570e38dd0f00b32817b7e14b516628a
aditya arrabally <aditya.arrabally@gmail.com>
2022-02-03 09:03:20 -0500

    Comitting new changes.

---
+++ JavaRefresher/src/main/java/com/learning/adi/java/javarefresher/functional/interfaces/Java8BiFunction4.java
@@ -0,0 +1,63 @@
+package com.learning.adi.java.javarefresher.functional.interfaces;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class Java8BiFunction4 {
+
+	public static void main(String[] args) {
+
+		Java8BiFunction4 obj = new Java8BiFunction4();
+		List<String> list = Arrays.asList("node", "c++", "java", "javascript");
+		List<String> result1 = obj.filterList(list, 3, (l1, size) -> {
+			if (l1.length() > size) {
+				return l1;
+			} else {
+				return null;
+			}
+		});
+
+		System.out.println(result1); // [node, java, javascript]
+
+		List<String> result2 = obj.filterList(list, "c", (l1, condition) -> {
+			if (l1.startsWith(condition)) {
+				return l1;
+			} else {
+				return null;
+			}
+		});
+
+		System.out.println(result2); // [c++]
+
+		List<Integer> number = Arrays.asList(1, 2, 3, 4, 5);
+
+		List<Integer> result3 = obj.filterList(number, 2, (l1, condition) -> {
+			if (l1 % condition == 0) {
+				return l1;
+			} else {
+				return null;
+			}
+		});
+
+		System.out.println(result3); // [2, 4]
+
+	}
+
+	public <T, U, R> List<R> filterList(List<T> list1, U condition, BiFunction<T, U, R> func) {
+
+		List<R> result = new ArrayList<>();
+
+		for (T t : list1) {
+			R apply = func.apply(t, condition);
+			if (apply != null) {
+				result.add(apply);
+			}
+		}
+
+		return result;
+
+	}
+
+}
