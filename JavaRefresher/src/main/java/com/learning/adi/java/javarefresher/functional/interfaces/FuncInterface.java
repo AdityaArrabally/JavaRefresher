24e77e7c4a550f63e5c3ef55080e2ddc7357c3c6
15186 <15186@RiyaAishani.lan>
2022-02-11 20:37:43 -0500

    Committing from new computer.

--- JavaRefresher/src/main/java/com/learning/adi/java/javarefresher/functional/interfaces/FuncInterface.java
+++ JavaRefresher/src/main/java/com/learning/adi/java/javarefresher/functional/interfaces/FuncInterface.java
@@ -3,7 +3,7 @@ package com.learning.adi.java.javarefresher.functional.interfaces;
 @FunctionalInterface
 public interface FuncInterface {
 	public default String HelloWorld() {
-		return "Hello World !!! ";
+		return "Hello World !!!! ";
 	}
 
 	public static String CustomMessage(String msg) {
