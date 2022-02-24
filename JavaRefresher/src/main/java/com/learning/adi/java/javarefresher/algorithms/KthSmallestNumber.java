bfc4f7806bc241dfa3154f4eaa60e8d75abf147b
15186 <15186@RiyaAishani.lan>
2022-02-23 20:56:09 -0500

    Fixing the merge issue.

--- JavaRefresher/src/main/java/com/learning/adi/java/javarefresher/algorithms/KthSmallestNumber.java
+++ JavaRefresher/src/main/java/com/learning/adi/java/javarefresher/algorithms/KthSmallestNumber.java
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 package com.learning.adi.java.javarefresher.algorithms;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
@@ -41,40 +39,3 @@ class KthSmallestNumber {
 		System.out.println("Kth smallest number is: " + result);
 	}
 }
-=======
-package com.learning.adi.java.javarefresher.algorithms;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
-class KthSmallestNumber {
-
-	public static int findKthSmallestNumber(int[] nums, int k) {
-		Set<Integer> hashSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-		TreeSet<Integer> treeSet = new TreeSet<Integer>();
-		treeSet.addAll(hashSet);
-		System.out.println(treeSet);
-		Iterator<Integer> iterator = treeSet.iterator();
-		int i = 0;
-		Integer current = null;
-		while (iterator.hasNext() && i < k) {
-			current = iterator.next();
-			i++;
-		}
-		return current;
-	}
-
-	public static void main(String[] args) {
-		int result = KthSmallestNumber.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 3);
-		System.out.println("Kth smallest number is: " + result);
-
-		// since there are two 5s in the input array, our 3rd and 4th smallest numbers
-		// should be a '5'
-		result = KthSmallestNumber.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 4);
-		System.out.println("Kth smallest number is: " + result);
-
-		result = KthSmallestNumber.findKthSmallestNumber(new int[] { 5, 12, 11, -1, 12 }, 3);
-		System.out.println("Kth smallest number is: " + result);
-	}
-}
->>>>>>> branch 'master' of https://github.com/AdityaArrabally/JavaRefresher.git
